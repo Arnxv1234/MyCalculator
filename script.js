5a016c560a1067847953069585d0384b704b83ed
Arnxv1234 <arnavrahman3@gmail.com>
2023-10-06 00:42:35 -0700

    FirstCalc

---
+++ script.js
@@ -0,0 +1,75 @@
+//javascript so the calculator is able to function
+
+class Calculator {
+    constructor() {
+        this.firstNumber = '';
+        this.secondNumber = '';
+        this.operator = '';
+        this.display = document.getElementById('display');
+    }
+
+    appendNumber(num) {
+        if (this.operator === '') {
+            this.firstNumber += num;
+            this.display.value = this.firstNumber;
+        } else {
+            this.secondNumber += num;
+            this.display.value = this.secondNumber;
+        }
+    }
+
+    setOperator(op) {
+        this.operator = op;
+    }
+
+    clearDisplay() {
+        this.firstNumber = '';
+        this.secondNumber = '';
+        this.operator = '';
+        this.display.value = '0';
+    }
+
+    calculate() {
+        const num1 = parseFloat(this.firstNumber);
+        const num2 = parseFloat(this.secondNumber);
+        let result = 0;
+
+        switch (this.operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+        }
+
+        this.display.value = result;
+        this.firstNumber = result.toString();
+        this.secondNumber = '';
+        this.operator = '';
+    }
+}
+
+const calculator = new Calculator();
+
+function getNumber(num) {
+    calculator.appendNumber(num);
+}
+
+function getOperator(op) {
+    calculator.setOperator(op);
+}
+
+function clearDisplay() {
+    calculator.clearDisplay();
+}
+
+function calculateEquals() {
+    calculator.calculate();
+}
